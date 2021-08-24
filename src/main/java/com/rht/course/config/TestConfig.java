d8e315b74bcec6724c2085984a2efc932acdad84
RHT <rodrigohisa@gmail.com>
2021-08-24 16:33:44 -0300

    Category

--- src/main/java/com/rht/course/config/TestConfig.java
+++ src/main/java/com/rht/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.rht.course.entities.Category;
 import com.rht.course.entities.Order;
 import com.rht.course.entities.User;
 import com.rht.course.entities.enums.OrderStatus;
+import com.rht.course.repositories.CategoryRepository;
 import com.rht.course.repositories.OrderRepository;
 import com.rht.course.repositories.UserRepository;
 
@@ -25,9 +27,17 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers"); 
+		
 		
 		User u1 = new User(null, "Rodrigo Hisashi", "rodrigohisa@gmail.com", "123456789", "1234567");
 		User u2 = new User(null, "Fernando Trinity", "fertrinity@gmail.com", "12326789", "1233567");
@@ -35,9 +45,11 @@ public class TestConfig implements CommandLineRunner{
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.CANCELED, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.PAID, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.PAID, u1); 
-		
+
+
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2,cat3));
 	}
 	
 	
---
+++ src/main/java/com/rht/course/services/CategoryService.java
@@ -0,0 +1,28 @@
+package com.rht.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rht.course.entities.Category;
+import com.rht.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+}
