9c4298039a68c6f40ba2c5bb21e408b4241ea69a
RHT <rodrigohisa@gmail.com>
2021-08-27 13:10:39 -0300

    Exception handling - delete

---
+++ src/main/java/com/rht/course/services/exception/DatabaseException.java
@@ -0,0 +1,10 @@
+package com.rht.course.services.exception;
+
+public class DatabaseException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+}
