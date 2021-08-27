569979117a121192411c3f4cb3c22c4e3401aa4f
RHT <rodrigohisa@gmail.com>
2021-08-27 11:37:26 -0300

    OrderItem Part 2

--- src/main/java/com/rht/course/config/TestConfig.java
+++ src/main/java/com/rht/course/config/TestConfig.java
@@ -10,69 +10,77 @@ import org.springframework.context.annotation.Profile;
 
 import com.rht.course.entities.Category;
 import com.rht.course.entities.Order;
+import com.rht.course.entities.OrderItem;
 import com.rht.course.entities.Product;
 import com.rht.course.entities.User;
 import com.rht.course.entities.enums.OrderStatus;
 import com.rht.course.repositories.CategoryRepository;
+import com.rht.course.repositories.OrderItemRepository;
 import com.rht.course.repositories.OrderRepository;
 import com.rht.course.repositories.ProductRepository;
 import com.rht.course.repositories.UserRepository;
 
 @Configuration
 @Profile("test")
-public class TestConfig implements CommandLineRunner{
-	
+public class TestConfig implements CommandLineRunner {
+
 	// Associar uma dependência fraca (desacoplada por meio da framework SpringBoot)
 	@Autowired
 	private UserRepository userRepository;
 
 	@Autowired
 	private OrderRepository orderRepository;
-	
+
 	@Autowired
 	private CategoryRepository categoryRepository;
-	
+
 	@Autowired
 	private ProductRepository productRepository;
 	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
-		
-		
+
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
-		Category cat3 = new Category(null, "Computers"); 
-		
+		Category cat3 = new Category(null, "Computers");
+
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
-		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2,cat3));
-		productRepository.saveAll(Arrays.asList(p1, p2, p3,p4, p5));
-		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
 		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
 		p2.getCategories().add(cat3);
 		p3.getCategories().add(cat3);
 		p4.getCategories().add(cat3);
 		p5.getCategories().add(cat2);
-		
-		productRepository.saveAll(Arrays.asList(p1, p2, p3,p4, p5));
-		
+
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
 		User u1 = new User(null, "Rodrigo Hisashi", "rodrigohisa@gmail.com", "123456789", "1234567");
 		User u2 = new User(null, "Fernando Trinity", "fertrinity@gmail.com", "12326789", "1233567");
-		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.CANCELED, u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.PAID, u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.PAID, u1); 
 
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.CANCELED, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.PAID, u1);
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
-	
-	
-	
+
 }
--- src/main/java/com/rht/course/entities/OrderItem.java
+++ src/main/java/com/rht/course/entities/OrderItem.java
@@ -7,6 +7,7 @@ import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.rht.course.entities.pk.OrderItemPK;
 
 @Entity
@@ -17,7 +18,7 @@ public class OrderItem implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	@EmbeddedId
-	private OrderItemPK id;
+	private OrderItemPK id = new OrderItemPK();
 	
 	private Integer quantity;
 	private Double price;
@@ -35,6 +36,7 @@ public class OrderItem implements Serializable{
 		
 	}
 
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
